df3fc756462705e152337fccd458def57e9002f2
slash1994bv <c.b.hirlav@gmail.com>
2017-07-27 17:05:18 +0300

    Database
    Code first training

---
+++ DataBaseProject/EntityFrameworkDatabaseFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkDatabaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new CarsDealerShipEntities();
+
+            //context.People
+        }
+    }
+}

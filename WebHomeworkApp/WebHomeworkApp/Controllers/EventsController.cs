0beace326dce8ca6852ef662f835b33ca00fe0cf
Claudiu Bogdan Hîrlav <c.b.hirlav@gmail.com>
2017-07-27 16:43:33 +0300

    First event application: events model, events controller, views and migrations

---
+++ WebHomeworkApp/WebHomeworkApp/Controllers/EventsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebHomeworkApp.Models;
+
+namespace WebHomeworkApp.Controllers
+{
+    public class EventsController : Controller
+    {
+        public static List<Models.Events> events = new List<Models.Events>();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(events);
+        }
+
+        public ActionResult Details()
+        {
+            Models.Events myEvent = new Models.Events() { Title = "Untold", Details = "Concert", Location = "Cluj", DateAndTime = new DateTime(2017, 07, 13) };
+            return View(myEvent);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Events myEvent)
+        {
+            if (myEvent.Title == null || myEvent.Title == String.Empty)
+            {
+                //var responseMessage = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
+                Response.StatusCode.Equals(404);
+                return View();
+            }
+            else
+            {
+                events.Add(myEvent);
+                return RedirectToAction("Details");
+            }
+        }
+    }
+}
---
+++ WebHomeworkApp/WebHomeworkApp/Models/Events.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebHomeworkApp.Models
+{
+    public class Events
+    {
+        // GET: Events
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        //[Required]
+        public string Details { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime DateAndTime { get; set; }
+
+        public string Image { get; set; }
+    }
+}
